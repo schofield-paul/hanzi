793450cf71c50ff5189fb3b4ef21669c04be97ea
Paul Schofield <paul.schofield.us@gmail.com>
2023-10-22 09:49:07 -0400

    implement sequential character animation

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -5,41 +5,54 @@ import HanziWriter from "hanzi-writer";
 import { data } from "../../data";
 import "./card.css";
 
-// Imports word, character list and animates using stroke order
 export default function Card() {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [showChinese, setShowChinese] = useState(false);
+  const [writers, setWriters] = useState([]);
 
   const showNextPair = () => {
-    const characterDiv = document.getElementById("character-target-div");
+    const characterDiv = document.getElementById("character-target-1");
     if (characterDiv) {
       characterDiv.innerHTML = "";
     }
 
     setCurrentIndex((prevIndex) => (prevIndex + 1) % data.length);
-    setShowChinese(false);
   };
 
   useEffect(() => {
-    let writer: HanziWriter;
-
-    // for each
-
-    if (showChinese) {
-      writer = data[currentIndex].chinese_character.map((character) => {
-        return HanziWriter.create("character-target-div", character, {
-          width: 100,
-          height: 100,
-          padding: 5,
-          showOutline: true,
-        });
+    // Create writers for the current data item
+    const newWriters = data[currentIndex].chinese_character.map((character) => {
+      return HanziWriter.create("character-target-1", character, {
+        width: 100,
+        height: 100,
+        padding: 5,
+        showCharacter: false,
+        showOutline: false,
       });
+    });
 
-      writer.forEach((writer) => {
-        writer.animateCharacter();
+    setWriters(newWriters);
+  }, [currentIndex]);
+
+  const animateCharacter = (index) => {
+    const delayBetweenAnimations = 1000;
+    const currentWriter = writers[index];
+
+    writers.forEach((writer) => {
+      writer.showOutline();
+    });
+
+    if (currentWriter) {
+      currentWriter.showCharacter();
+      currentWriter.showOutline();
+      currentWriter.animateCharacter({
+        onComplete: function () {
+          setTimeout(function () {
+            animateCharacter(index + 1); // Start animating the next character
+          }, delayBetweenAnimations);
+        },
       });
     }
-  }, [currentIndex, showChinese]);
+  };
 
   return (
     <>
@@ -48,8 +61,8 @@ export default function Card() {
         <button onClick={showNextPair}>Next Pair</button>
       </div>
       <div className="pair-2">
-        <div id="character-target-div"></div>
-        <button onClick={() => setShowChinese(true)}>Animate</button>
+        <div id="character-target-1"></div>
+        <button onClick={() => animateCharacter(0)}>Animate</button>
       </div>
     </>
   );
