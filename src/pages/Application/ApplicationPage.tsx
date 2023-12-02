02b7e47b87731f1c4a11421b3c834f24d88a62b8
Paul Schofield <paul.schofield.us@gmail.com>
2023-12-01 19:35:32 -0500

    light styling

--- src/pages/Application/ApplicationPage.tsx
+++ src/pages/Application/ApplicationPage.tsx
@@ -2,6 +2,8 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import Card from "../../components/cards/Card";
+import classNames from "classnames";
+import style from "./ApplicationPage.module.css";
 
 export default function Application() {
   const location = useLocation();
@@ -9,11 +11,13 @@ export default function Application() {
 
   return (
     <div>
-      <h1>Application</h1>
+      <h1 className={classNames(style.h1)}>App</h1>
       {data ? (
         <>
-          <pre>{JSON.stringify(data, null, 2)}</pre>
           <Card data={data} />
+          <h3>Raw Data</h3>
+
+          <pre>{JSON.stringify(data, null, 2)}</pre>
         </>
       ) : (
         <p>No data available</p>
