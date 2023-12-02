fe85714e6b328b7ed070012aec5bc63d73d23458
Paul Schofield <paul.schofield.us@gmail.com>
2023-12-01 19:35:32 -0500

    light styling

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,3 +1,12 @@
+import Selections from "../Selection/SelectionPage";
+import classNames from "classnames";
+import style from "./HomePage.module.css";
+
 export default function Homepage() {
-  return <div className="homepage">"Hey there!"</div>;
+  return (
+    <div className={classNames(style.app)}>
+      <h1>Hanzi</h1>
+      <Selections />
+    </div>
+  );
 }
