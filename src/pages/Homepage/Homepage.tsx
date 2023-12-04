baa38b2dcd9051d1757b60a3dd94bfa5868292b3
Paul Schofield <paul.schofield.us@gmail.com>
2023-12-04 09:42:14 -0500

    write css small screen logic

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,18 +1,20 @@
 import Selections from "../Selection/SelectionPage";
 import classNames from "classnames";
 import style from "./HomePage.module.css";
-import Banner from "../../components/banner/Banner";
 
 export default function Homepage() {
   return (
     <div className={classNames(style.app)}>
       <h1>Hanzi</h1>
+      <div className={classNames("hero", style.hero)}>
+        <h1 className={classNames("title", style.title)}>
+          The best way to learn Chinese characters
+        </h1>
+        <h2 className={classNames("subtitle", style.subtitle)}>
+          Learn Chinese characters with ease
+        </h2>
+      </div>
 
-      <Banner
-        title="The best way to learn Chinese characters"
-        subtitle="Learn Chinese characters with ease"
-        image="https://images.unsplash.com/photo-1562887108-2e5a3d8e9b7b"
-      />
       <Selections />
     </div>
   );
