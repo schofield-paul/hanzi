10f7ea22e2ffc117cb450b2eb56f6ce0459a70a8
Paul Schofield <paul.schofield.us@gmail.com>
2023-12-11 19:36:28 -0500

    add landing image and update subtext

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,17 +1,23 @@
 import Selections from "../Selection/SelectionPage";
 import classNames from "classnames";
 import style from "./HomePage.module.css";
+import landingImage from "../../assets/landing.jpeg";
 
 export default function Homepage() {
   return (
     <div className={classNames(style.app)}>
       <h1>Hanzi</h1>
       <div className={classNames("hero", style.hero)}>
+        <img
+          src={landingImage}
+          className={classNames(style.landingImage)}
+          alt="Landing"
+        />
         <h1 className={classNames("title", style.title)}>
           The best way to learn Chinese characters
         </h1>
         <h2 className={classNames("subtitle", style.subtitle)}>
-          Learn Chinese characters with ease
+          Become fluent with stroke order practice
         </h2>
       </div>
 
