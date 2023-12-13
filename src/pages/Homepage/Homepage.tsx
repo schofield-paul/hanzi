f75e9c35464a1fe5c970617edf8680da9773d7e7
Paul Schofield <paul.schofield.us@gmail.com>
2023-12-13 10:39:14 -0500

    Revert "Merge pull request #28 from schofield-paul/feature/small-refactor-section-page"
    This reverts commit 9cc2f1106e9105b39bcc440f1745b67ed1f67838, reversing
    changes made to eb6dd4c5a6a98a7d2433d2056d44767a797725fa.

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,4 +1,4 @@
-import SelectStageSection from "../../sections/SelectStage/SelectStageSection";
+import Selections from "../Selection/SelectionPage";
 import classNames from "classnames";
 import style from "./HomePage.module.css";
 import landingImage from "../../assets/landing.jpeg";
@@ -21,7 +21,7 @@ export default function Homepage() {
         </h2>
       </div>
 
-      <SelectStageSection />
+      <Selections />
     </div>
   );
 }
