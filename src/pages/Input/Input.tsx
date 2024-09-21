6f3ee94442421374ddab3f20e1ded125c5e6c22c
Paul Schofield <paul.schofield.us@gmail.com>
2024-09-21 17:12:54 -0400

    remove commented code

--- src/pages/Input/Input.tsx
+++ src/pages/Input/Input.tsx
@@ -2,12 +2,6 @@ import style from "./Input.module.css";
 import { useRef, useState, useEffect } from "react";
 import HanziWriter from "hanzi-writer";
 
-// const fetchDataTest = async () => {
-//   const response = await fetch("http://localhost:3005/translation");
-//   console.log(response);
-// };
-//test
-
 export default function Input() {
   const [inputValue, setInputValue] = useState<string>("");
   const writerContainerRef = useRef<HTMLDivElement | null>(null);
@@ -38,20 +32,13 @@ export default function Input() {
     }
   };
 
-  // useEffect(() => {
-  //   // This will only run when the Input component is mounted
-  //   fetchDataTest();
-  // }, []);
-
-  // Function to detect if input contains English characters
   const containsEnglish = (input: string) => {
-    const englishRegex = /^[a-zA-Z\s]+$/; // Regular expression for English letters and spaces
+    const englishRegex = /^[a-zA-Z\s]+$/;
     return englishRegex.test(input);
   };
 
   // Handle form submission
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    // TODO: call the translation API
     // TODO: check that the input is Mandarin; maybe prevent non-Mandarin input?
     // (Also, maybe support both English -> Mandarin and Mandarin -> English?)
     e.preventDefault();
