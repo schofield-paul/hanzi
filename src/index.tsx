95b76df86d2bf3b5640970ad65248c5f5c267469
Paul Schofield <paul.schofield.us@gmail.com>
2024-09-28 08:45:23 -0400

    initialize Google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 // @ts-nocheck
 
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useLocation,
+} from "react-router-dom";
 import Layout from "./layout";
 import Application from "./pages/Application/ApplicationPage";
 import Homepage from "./pages/Homepage/Homepage";
@@ -12,46 +16,73 @@ import About from "./pages/About/AboutPage";
 import Input from "./pages/Input/Input";
 import SelectStageSection from "./sections/SelectStage/SelectStageSection";
 
-const router = createBrowserRouter([
-  {
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Homepage />,
-        errorElement: <div>404 Not Found</div>,
-      },
-      {
-        path: "/app",
-        element: <Application />,
-        errorElement: <div>404 Not Found</div>,
-      },
-      {
-        path: "/selection",
-        element: <SelectStageSection />,
-        errorElement: <div>404 Not Found</div>,
-      },
-      {
-        path: "/input",
-        element: <Input />,
-        errorElement: <div>404 Not Found</div>,
-      },
+const useGoogleAnalytics = () => {
+  const location = useLocation();
 
-      {
-        path: "/about",
-        element: <About />,
-        errorElement: <div>404 Not Found</div>,
-      },
-    ],
-  },
-]);
+  useEffect(() => {
+    // Load Google Analytics script
+    const script = document.createElement("script");
+    script.src = `https://www.googletagmanager.com/gtag/js?id=G-W86TED8WXD`;
+    script.async = true;
+    document.head.appendChild(script);
+
+    // Initialize Google Analytics
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "G-W86TED8WXD");
+
+    // Track pageview on route change
+    gtag("config", "G-W86TED8WXD", {
+      page_path: location.pathname,
+    });
+  }, [location]);
+};
+
+function App() {
+  useGoogleAnalytics();
+
+  const router = createBrowserRouter([
+    {
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Homepage />,
+          errorElement: <div>404 Not Found</div>,
+        },
+        {
+          path: "/app",
+          element: <Application />,
+          errorElement: <div>404 Not Found</div>,
+        },
+        {
+          path: "/selection",
+          element: <SelectStageSection />,
+          errorElement: <div>404 Not Found</div>,
+        },
+        {
+          path: "/input",
+          element: <Input />,
+          errorElement: <div>404 Not Found</div>,
+        },
+        {
+          path: "/about",
+          element: <About />,
+          errorElement: <div>404 Not Found</div>,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<RouterProvider router={router} />);
+root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
