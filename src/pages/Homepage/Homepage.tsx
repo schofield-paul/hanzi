a3d61f75cdddd460922e6877eb3e6b90cbb77b22
Paul Schofield <paul.schofield.us@gmail.com>
2024-09-29 15:59:02 -0400

    homepage UI rework

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -9,29 +9,26 @@ export default function Homepage() {
   return (
     <>
       <div className={style.wrapper}>
+        <p className={style.logoReflection}>Generated characters</p>
+        <p className={style.preText}>Hanzi App</p>
         <div className={style.app}>
           <h1 className={classNames(style.title)}>
-            <span className={classNames(style.gradientText)}>
-              {" "}
-              Learn Chinese characters with ease
-            </span>
+            Learn Chinese. See progress.
+            <br />
+            <span className={style.centerText}>Achieve fluency.</span>
           </h1>
           <h2 className={classNames("subtitle", style.subtitle)}>
-            Hanzi is a tool for learning Chinese characters through stroke order
-            writing
+            Simple to start. Fun to learn. Dive into Chinese characters
+            <br />
+            and fluency with interactive animations and words.
           </h2>
-          <Link to="/input">
-            <button className={classNames("button", style.button)}>
-              Get Started
-            </button>
-          </Link>
-
-          <div className={classNames("hero", style.hero)}>
-            <img
-              src={heroImage}
-              className={classNames(style.heroImage)}
-              alt="Hero"
-            />
+          <div className={style.buttons}>
+            <Link to="/input">
+              <button className={style.button}>Get Started</button>
+            </Link>
+            <Link to="/about">
+              <button className={style.learnMore}>Learn More →</button>
+            </Link>
           </div>
         </div>
         <Quote />
