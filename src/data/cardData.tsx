ef807777a1c1701a4d3189df5855ece17dbc2222
Paul Schofield <paul.schofield.us@gmail.com>
2024-10-07 17:12:40 -0400

    homepage code cleanup

---
+++ src/data/cardData.tsx
@@ -0,0 +1,103 @@
+const allCardData = [
+  {
+    title: "Card 5",
+    content: "What do you do for work?",
+  },
+  {
+    title: "Card 6",
+    content: "Do you speak English?",
+  },
+  {
+    title: "Card 8",
+    content: "How do I get to the subway station?",
+  },
+  {
+    title: "Card 1",
+    content: "Where are the best local cuisines?",
+  },
+  {
+    title: "Card 3",
+    content:
+      "This dish looks amazing! Can you tell me what ingredients are in it?",
+  },
+  {
+    title: "Card 2",
+    content: "Hello, my name is",
+  },
+  {
+    title: "Card 7",
+    content: "How are you? ",
+  },
+  {
+    title: "Card 4",
+    content: "Nice to meet you. Where is the best tourist attraction?",
+  },
+  {
+    title: "Card 9",
+    content: "What is the weather like today?",
+  },
+  {
+    title: "Card 10",
+    content: "What time is it?",
+  },
+  {
+    title: "Card 11",
+    content: "What is the capital of China?",
+  },
+  {
+    title: "Card 12",
+    content: "What’s the most popular dish here?",
+  },
+  {
+    title: "Card 13",
+    content: "How far is it from here?",
+  },
+  {
+    title: "Card 14",
+    content: "How much does this cost?",
+  },
+  {
+    title: "Card 15",
+    content: "Can you tell me about this place’s history?",
+  },
+  {
+    title: "Card 16",
+    content: "What is the best time to visit?",
+  },
+  {
+    title: "Card 17",
+    content: "When is the next train?",
+  },
+  {
+    title: "Card 18",
+    content: "Do you have any recommendations for local music or shows? ",
+  },
+  {
+    title: "Card 19",
+    content: "Do you have any hobbies? ",
+  },
+  {
+    title: "Card 20",
+    content: "Do you watch TV shows? ",
+  },
+  {
+    title: "Card 21",
+    content: "I'm primarily studying simplified characters. ",
+  },
+  {
+    title: "Card 22",
+    content: "Where is the nearest bookstore.",
+  },
+  {
+    title: "Card 23",
+    content: "What do you think of this place? ",
+  },
+  {
+    title: "Card 24",
+    content: "That’s an interesting idea! ",
+  },
+];
+
+export const cardData = allCardData.slice(0, 8);
+export const cardData2 = allCardData.slice(8, 16);
+export const cardData3 = allCardData.slice(16);
