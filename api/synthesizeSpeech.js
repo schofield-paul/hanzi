9e9d9887dc2962642ce4214e77ec2d57ad97b614
Paul Schofield <paul.schofield.us@gmail.com>
2024-10-13 18:16:08 -0400

    Update text to speech api key access

--- api/synthesizeSpeech.js
+++ api/synthesizeSpeech.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const client = new textToSpeech.TextToSpeechClient({
   credentials: {
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    private_key: process.env.GOOGLE_PRIVATE_KEY,
   },
   projectId: process.env.GOOGLE_PROJECT_ID,
 });
