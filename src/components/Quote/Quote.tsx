c901ae1a894a018662ce470bd9924ced42c4fa93
Paul Schofield <paul.schofield.us@gmail.com>
2024-10-16 05:50:58 -0400

    formatting quote on homepage

--- src/components/Quote/Quote.tsx
+++ src/components/Quote/Quote.tsx
@@ -5,11 +5,14 @@ export default function Quote() {
   return (
     <div>
       <div className={style.quoteWrapper}>
-        <p className={style.quoteText}>
-          "Language is our most powerful invention. It has allowed us to
-          transcend time and space, to store knowledge outside of our brains,
-          and to build complex societies." — Yuval Noah Harari
-        </p>
+        <div className={style.quoteContent}>
+          <p className={style.quoteText}>
+            "Language is our most powerful invention. It has allowed us to
+            transcend time and space, to store knowledge outside of our brains,
+            and to build complex societies."
+          </p>
+          <p className={style.quoteAuthor}>— Yuval Noah Harari</p>
+        </div>
       </div>
     </div>
   );
